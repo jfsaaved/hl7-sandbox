f2b363d49f94d6d6ed1a65fe89e05b632d780443
jfsaaved <julian.felipe.saavedra@gmail.com>
2019-11-26 13:17:27 -0500

    Working Base

---
+++ src/test/java/io/accenture/hl7sandbox/Hl7SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.accenture.hl7sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hl7SandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
